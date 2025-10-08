53484ed84b35efeccda346a09da52679a62a5f81
Stan-21 <77824280+Stan-21@users.noreply.github.com>
2025-10-08 21:48:52 +0000

    purchasing an upgrade (step 5 complete)

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,49 @@ document.body.innerHTML = `
   <h1> CMPM 121 Project </h1>
   <p> Rocks Collected: <span id="counter"> 0 </span></p>
   <button id = "button"> This is a button 🪨 </button>
+  <button id = "autoUpgrade"> Upgrade Auto Mine </button>
 `;
 
 const counterElement = document.getElementById("counter")!;
 const buttonElement = document.getElementById("button")!;
+const upgradeElement = document.getElementById("autoUpgrade")!;
 
 let counter = 0;
-let last = performance.now();
 
 buttonElement.addEventListener("click", () => {
   click();
 });
 
-function autoClick() {
-  if ((performance.now() - last) > 1000) {
-    click();
-    last = performance.now();
+upgradeElement.addEventListener("click", () => {
+  autoMine = true;
+  counter -= 10;
+  interval = Math.ceil(interval / 2);
+});
+
+function tick() {
+  if (counter < 10) {
+    (upgradeElement as HTMLButtonElement).disabled = true;
+  } else {
+    (upgradeElement as HTMLButtonElement).disabled = false;
   }
-  requestAnimationFrame(autoClick);
+  autoClick();
+  requestAnimationFrame(tick);
+}
+
+let lastTime = performance.now();
+let autoMine = false; // Auto mine flag
+let interval = 2000;
+
+function autoClick() {
+  const currentTime = performance.now();
+  const elapsedTime = currentTime - lastTime;
+
+  if (elapsedTime >= interval) {
+    if (autoMine) {
+      click();
+    }
+    lastTime = currentTime - (elapsedTime % interval);
+  }
 }
 
 function click() {
@@ -29,6 +54,6 @@ function click() {
   counterElement.innerHTML = counter.toString();
 }
 
-requestAnimationFrame(autoClick);
+requestAnimationFrame(tick);
 
 console.log("my internet is no longer dead lets goooo");
