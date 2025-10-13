427040714633ef3d4dbae1145d3ed2665b749fa3
Stan-21 <77824280+Stan-21@users.noreply.github.com>
2025-10-13 09:08:14 +0000

    content expansion (added two more upgrades: one normal upgrade and one 'secret' upgrade AND a description tooltip for each item)  (step 10 complete)

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,28 @@ document.body.innerHTML = `
   <button id = "rock"> 🪨 </button>
   <button id = "button"> ⛏️⛏️ </button>
   <p>
-    <button id = "pickUpgrade" class = "upgrade"> Auto Pick (10 rocks) (x0) </button>
-    <button id = "drillUpgrade" class = "upgrade"> Driller (100 rocks) (x0) </button>
-    <button id = "bombUpgrade" class = "upgrade"> Bomber (1000 rocks) (x0) </button>
+    <div class = "tooltip">
+      <button id = "pickUpgrade" class = "upgrade"> Auto Pick (10 rocks) (x0) </button>
+      <span class = "tooltiptext"> An auto pick! +0.1 rocks per second </span>
+    </div>
+    <div class = "tooltip">
+      <button id = "drillUpgrade" class = "upgrade"> Driller (100 rocks) (x0) </button>
+      <span class = "tooltiptext"> Drill goes burr. +2 rocks per second </span>
+    </div>
+    <div class = "tooltip">
+      <button id = "bombUpgrade" class = "upgrade"> Bomber (1000 rocks) (x0) </button>
+      <span class = "tooltiptext"> Small boom.  + 50 rocks per second </span>
+    </div>
+    <div class = "tooltip">
+      <button id = "bigUpgrade" class = "upgrade"> Bigger Bomber (5000 rocks) (x0) </button>
+      <span class = "tooltiptext"> Bigger boom.  + 400 rocks per second </span>
+    </div>
   </p>
 `;
 
 interface Upgrade {
   name: string;
+  description: string;
   cost: number;
   rate: number;
   count: number;
@@ -28,11 +42,41 @@ const buttonElement = document.getElementById("button")!;
 const pickUpgrade = document.getElementById("pickUpgrade")!;
 const drillUpgrade = document.getElementById("drillUpgrade")!;
 const bombUpgrade = document.getElementById("bombUpgrade")!;
+const biggerUpgrade = document.getElementById("bigUpgrade")!;
 
 const availableItems: Upgrade[] = [
-  { name: "Auto Pick", cost: 10, rate: 0.1, count: 0, element: pickUpgrade },
-  { name: "Driller", cost: 100, rate: 2, count: 0, element: drillUpgrade },
-  { name: "Bomber", cost: 1000, rate: 50, count: 0, element: bombUpgrade },
+  {
+    name: "Auto Pick",
+    description: "An auto pick!  Slow, but does the trick!",
+    cost: 10,
+    rate: 0.1,
+    count: 0,
+    element: pickUpgrade,
+  },
+  {
+    name: "Driller",
+    description: "Drill goes burrr",
+    cost: 100,
+    rate: 2,
+    count: 0,
+    element: drillUpgrade,
+  },
+  {
+    name: "Bomber",
+    description: "Small boom",
+    cost: 1000,
+    rate: 50,
+    count: 0,
+    element: bombUpgrade,
+  },
+  {
+    name: "Bigger Bomber",
+    description: "Bigger boom",
+    cost: 5000,
+    rate: 400,
+    count: 0,
+    element: biggerUpgrade,
+  },
 ];
 
 availableItems.forEach((item) => {
@@ -46,11 +90,15 @@ availableItems.forEach((item) => {
   });
 });
 
-let counter = 0;
+let counter = 9999; // current money / score
+let finalUpgradeFlag = false;
 
 buttonElement.addEventListener("click", () => {
   click();
 });
+
+let yesButton: HTMLButtonElement;
+let noButton: HTMLButtonElement;
 
 function tick() {
   for (const i of availableItems) {
@@ -59,6 +107,36 @@ function tick() {
     } else {
       (i.element as HTMLButtonElement).disabled = true;
     }
+  }
+
+  if (counter >= 10000 && !finalUpgradeFlag) {
+    finalUpgradeFlag = true;
+    const newText = document.createElement("div");
+    newText.textContent =
+      "A new possible upgrade appears...  It's an even bigger bomb.  Buy it (10000 rocks)?  ";
+    document.body.append(newText);
+
+    yesButton = document.createElement("button");
+    yesButton.addEventListener("click", () => {
+      counter -= 10000;
+      document.body.innerHTML =
+        "BOOM!  The mine blows up.  Nothing else can be mined";
+    });
+    yesButton.textContent = " yes ";
+    newText.append(yesButton);
+    noButton = document.createElement("button");
+    noButton.addEventListener("click", () => {
+      newText.innerHTML = "";
+      newText.innerHTML =
+        "The bomb fades away and is never seen again.  The idea of what could've been lingers in your head...";
+    });
+    noButton.textContent = " no ";
+    newText.append(noButton);
+  }
+
+  if (finalUpgradeFlag && counter < 10000) {
+    yesButton.disabled = true;
+    noButton.disabled = true;
   }
   autoClick();
   requestAnimationFrame(tick);
